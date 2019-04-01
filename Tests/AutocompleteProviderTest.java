14af69a65c62c07e89c5babd744b1c7e5d792205
jtwigg978 <39139006+jtwigg978@users.noreply.github.com>
2019-04-01 14:53:33 -0400

    Add files via upload

---
+++ Tests/AutocompleteProviderTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutocompleteProviderTest {
+    List<Candidate> empty = new LinkedList<Candidate>();
+
+    // Testing with empty string
+    @Test
+    public void emptyTrain() {
+        AutocompleteProvider auto = new AutocompleteProvider();
+
+        auto.train("");
+        assertEquals(auto.getWords("The"), empty);
+    }
+
+    @Test
+    public void emptyFragment() {
+        AutocompleteProvider auto = new AutocompleteProvider();
+
+        auto.train("What happens if I don't enter a fragment at all?");
+        assertEquals(auto.getWords(""), empty);
+    }
+
+    @Test
+    public void specialCharacters() {
+        AutocompleteProvider auto = new AutocompleteProvider();
+        List<Candidate> special = new LinkedList<Candidate>(Arrays.asList(new Candidate("don't", 2),
+                new Candidate("do-nt", 1)));
+
+        auto.train("I am testing don't, do-nt, and don't again.");
+        assertEquals(auto.getWords("do"), special);
+    }
+
+    @Test
+    public void capsTest() {
+        AutocompleteProvider auto = new AutocompleteProvider();
+        List<Candidate> caps = new LinkedList<Candidate>(Arrays.asList(new Candidate("testing", 2),
+                new Candidate("term", 1)));
+
+        auto.train("Testing that the testing term has two confidence.");
+        assertEquals(auto.getWords("te"), caps);
+    }
+
+}
